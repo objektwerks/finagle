573d434512bdb76c2d1867940dbd1c34d890ea2c
objektwerks <objektwerks@runbox.com>
2021-02-13 09:58:52 -0500

    corrected now server test

--- src/test/scala/objektwerks/NowServerTest.scala
+++ src/test/scala/objektwerks/NowServerTest.scala
@@ -1,27 +1,32 @@
 package objektwerks
 
 import com.twitter.finagle.Http
-import com.twitter.util.{Await, Duration, Return, Throw}
+import com.twitter.util.{Return, Throw}
 import com.typesafe.config.ConfigFactory
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-trait TestNowServer {
+class NowServerTest extends AnyFunSuite with Matchers {
   val conf = ConfigFactory.load("test.conf")
   val host = conf.getString("host")
   val port = conf.getString("port")
 
-  Await.ready( Http.serve(port, NowService.newInstance), Duration.fromSeconds(9) )
-  ()
-}
+  val client = NowClient.newInstance(host, port)
+  val server = Http.serve(port, NowService.newInstance)
 
-class NowServerTest extends AnyFunSuite with Matchers with TestNowServer {
+  sys.addShutdownHook {
+    client.close()
+    server.close()
+    ()
+  }
+
   test("now") {
-    val client = NowClient.newInstance(host, port)
     val request = NowClient.newRequest(host)
     client(request).respond {
-      case Return(value) => println(s"*** now result: $value ***")
+      case Return(response) =>
+        response.contentString.nonEmpty shouldBe true
+        println(s"*** NowServer response: ${response.contentString}")
       case Throw(error) => fail(error)
     }
   }
