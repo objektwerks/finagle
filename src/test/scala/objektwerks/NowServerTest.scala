e9b36ac399c2562c4034e4179d74e57f2edaa26f
objektwerks <objektwerks@runbox.com>
2021-02-13 08:48:32 -0500

    edited now server test

--- src/test/scala/objektwerks/NowServerTest.scala
+++ src/test/scala/objektwerks/NowServerTest.scala
@@ -3,25 +3,18 @@ package objektwerks
 import com.twitter.finagle.Http
 import com.twitter.util.{Await, Duration, Return, Throw}
 import com.typesafe.config.ConfigFactory
-
-import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class NowServerTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
+class NowServerTest extends AnyFunSuite with Matchers {
   val conf = ConfigFactory.load("test.conf")
   val host = conf.getString("host")
   val port = conf.getString("port")
 
-  val server = Await.ready( Http.serve(port, NowService.newInstance), Duration.fromSeconds(3) )
-  val client = NowClient.newInstance(host, port)
-
-  override protected def afterAll(): Unit = {
-    client.close()
-    server.close()
-  }
+  Await.ready( Http.serve(port, NowService.newInstance), Duration.fromSeconds(9) )
 
   test("now") {
+    val client = NowClient.newInstance(host, port)
     val request = NowClient.newRequest(host)
     client(request).respond {
       case Return(value) => println(s"*** now result: $value ***")
