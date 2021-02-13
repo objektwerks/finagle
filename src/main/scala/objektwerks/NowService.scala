0db0e1c7b0c2d6d59c983d3af597b7d8eeb97ab5
objektwerks <objektwerks@runbox.com>
2021-02-13 08:17:02 -0500

    added now service

---
+++ src/main/scala/objektwerks/NowService.scala
@@ -0,0 +1,16 @@
+package objektwerks
+
+import com.twitter.finagle.Service
+import com.twitter.finagle.http._
+import com.twitter.util.Future
+
+import java.time.Instant
+
+object NowService {
+  def newInstance: Service[Request, Response] =
+    (request: Request) => Future.value {
+      val response = Response(request.version, Status.Ok)
+      response.contentString = Instant.now.toString
+      response
+    }
+}
