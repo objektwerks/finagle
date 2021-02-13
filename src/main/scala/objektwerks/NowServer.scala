2415745873cef46295cc19d2cfdf5c8c9f56286c
objektwerks <objektwerks@runbox.com>
2021-02-13 08:34:43 -0500

    edited readme

--- src/main/scala/objektwerks/NowServer.scala
+++ src/main/scala/objektwerks/NowServer.scala
@@ -1,17 +1,15 @@
 package objektwerks
 
 import com.twitter.finagle.Http
-import com.twitter.util.{Await, Duration}
+import com.twitter.util.Await
 import com.typesafe.config.ConfigFactory
 
 object NowServer {
-  val conf = ConfigFactory.load("now.conf")
-  val port = conf.getString("port")
-
   def main(args: Array[String]): Unit = {
-    val server = Await.ready( Http.serve(port, NowService.newInstance) )
-    sys.addShutdownHook(
-      Await.result(server.close, Duration.fromSeconds(1))
-    )
+    val conf = ConfigFactory.load("now.conf")
+    val port = conf.getString("port")
+
+    Await.ready( Http.serve(port, NowService.newInstance) )
+    ()
   }
 }
