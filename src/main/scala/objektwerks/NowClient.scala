285490eae5154947c321d0e789f07c33de75928a
objektwerks <objektwerks@runbox.com>
2021-02-13 08:17:31 -0500

    added now client

---
+++ src/main/scala/objektwerks/NowClient.scala
@@ -0,0 +1,14 @@
+package objektwerks
+
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http._
+
+object NowClient {
+  def newInstance(host: String, port: String): Service[Request, Response] =  Http.newService(s"$host$port")
+
+  def newRequest(host: String): Request = {
+    val request = Request(Method.Get, "/")
+    request.host(host)
+    request
+  }
+}
