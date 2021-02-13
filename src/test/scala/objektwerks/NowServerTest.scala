b3565221622fb0b081c6155c27032dd0824cf285
objektwerks <objektwerks@runbox.com>
2021-02-13 08:18:04 -0500

    added now server test

---
+++ src/test/scala/objektwerks/NowServerTest.scala
@@ -0,0 +1,31 @@
+package objektwerks
+
+import com.twitter.finagle.Http
+import com.twitter.util.{Await, Duration, Return, Throw}
+import com.typesafe.config.ConfigFactory
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class NowServerTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
+  val conf = ConfigFactory.load("now.conf")
+  val host = conf.getString("host")
+  val port = conf.getString("port")
+
+  val server = Await.ready( Http.serve(port, NowService.newInstance), Duration.fromSeconds(3) )
+  val client = NowClient.newInstance(host, port)
+
+  override protected def afterAll(): Unit = {
+    client.close()
+    server.close()
+  }
+
+  test("now") {
+    val request = NowClient.newRequest(host)
+    client(request).respond {
+      case Return(value) => println(s"*** now result: $value ***")
+      case Throw(error) => fail(error)
+    }
+  }
+}
