9e88fda067c3b7c0b82bfe733757f599333147ef
objektwerks <objektwerks@runbox.com>
2021-02-16 13:24:26 -0500

    added newMessage def to echo client

--- src/main/scala/objektwerks/EchoClient.scala
+++ src/main/scala/objektwerks/EchoClient.scala
@@ -3,12 +3,16 @@ package objektwerks
 import com.twitter.finagle.Thrift
 import com.twitter.util.Future
 
+import java.time.Instant
+
 object EchoClient {
   def apply(host: String, port: String): EchoClient = new EchoClient(host, port)
 }
 
 class EchoClient(val host: String, val port: String) {
   private val methodPerEndpoint = Thrift.client.build[EchoService.MethodPerEndpoint](s"$host$port")
+
+  def newMessage: String = Instant.now.toString
 
   def sendMessage(message: String): Future[String] = methodPerEndpoint.echo(message)
 
