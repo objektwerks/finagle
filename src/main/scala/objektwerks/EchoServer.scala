4c9ca64124fddee1c55f7a18530a1d7a4405bbc4
objektwerks <objektwerks@runbox.com>
2021-02-16 13:25:30 -0500

    added echo client newMessage call to echo server

--- src/main/scala/objektwerks/EchoServer.scala
+++ src/main/scala/objektwerks/EchoServer.scala
@@ -4,8 +4,6 @@ import com.twitter.finagle.Thrift
 import com.twitter.util.{Future, Return, Throw}
 import com.typesafe.config.ConfigFactory
 
-import java.time.Instant
-
 class EchoServer extends EchoService.MethodPerEndpoint {
   override def echo(message: String): Future[String] = Future.value(message)
 }
@@ -19,7 +17,7 @@ object EchoServer {
     val server = Thrift.server.serveIface(s"$host$port", new EchoServer())
     val client = EchoClient(host, port)
 
-    val message = Instant.now.toString
+    val message = client.newMessage
     client.sendMessage(message).respond {
       case Return(response) => println(s"*** EchoClient ($message) to/from EchoServer ($response)")
       case Throw(error) => println(s"Failure: ${error.getMessage}")
