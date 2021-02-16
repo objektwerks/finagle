2cf90daa62236a7956c6b14f162f3c5cd5983b63
objektwerks <objektwerks@runbox.com>
2021-02-16 08:59:04 -0500

    added echo client to echo server for live testing

--- src/main/scala/objektwerks/EchoServer.scala
+++ src/main/scala/objektwerks/EchoServer.scala
@@ -1,8 +1,10 @@
 package objektwerks
 
 import com.twitter.finagle.Thrift
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Await, Future, Return, Throw}
 import com.typesafe.config.ConfigFactory
+
+import java.time.Instant
 
 class EchoServer extends EchoService.MethodPerEndpoint {
   override def echo(message: String): Future[String] = Future.value(message)
@@ -15,9 +17,18 @@ object EchoServer {
     val port = conf.getString("port")
 
     val server = Thrift.server.serveIface(s"$host$port", new EchoServer())
+
+    val client = EchoClient(host, port)
+    val message = Instant.now.toString
+    client.sendMessage(message).respond {
+      case Return(response) => println(s"*** EchoClient ($message) to/from EchoServer ($response)")
+      case Throw(error) => println(s"Failure: ${error.getMessage}")
+    }
+
     Await.ready(server)
 
     sys.addShutdownHook {
+      client.close()
       server.close()
       ()
     }
