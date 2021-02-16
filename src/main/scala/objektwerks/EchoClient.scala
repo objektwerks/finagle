9827ae211f2806c4900e14045ba8a75de3981b82
objektwerks <objektwerks@runbox.com>
2021-02-16 08:24:23 -0500

    added EchoClient

---
+++ src/main/scala/objektwerks/EchoClient.scala
@@ -0,0 +1,21 @@
+package objektwerks
+
+import com.twitter.finagle.Thrift
+import com.twitter.util.Future
+
+import java.time.Instant
+
+object EchoClient {
+  def apply(host: String, port: String): EchoClient = new EchoClient(host, port)
+}
+
+class EchoClient(val host: String, val port: String) {
+  val methodPerEndpoint = Thrift.client.build[EchoService.MethodPerEndpoint](s"$host$port")
+
+  def sendRequest: Future[String] = methodPerEndpoint.echo(s"echo service: ${Instant.now.toString}")
+
+  def close(): Unit = {
+    methodPerEndpoint.asClosable.close()
+    ()
+  }
+}
